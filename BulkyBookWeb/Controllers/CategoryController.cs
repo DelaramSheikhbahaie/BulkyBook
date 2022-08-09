6e055b12e1e7c661cc37cb6ccc7149cd92af1df4
Delaram Sheikhbahaie <sheikhbahaied@gmail.com>
2022-08-09 12:06:25 +0430

    create category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -17,5 +17,28 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> ObjCategoryList = _db.categories;
             return View(ObjCategoryList);
         }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", " Name and DO cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("index");
+
+            }
+            return View(obj);
+        }
     }
 }
